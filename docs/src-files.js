5b380c7cbaae9b124cdf73060e3de07d716f4fdd
PedroGGBM <PedroGGBM@users.noreply.github.com>
2024-02-20 14:32:51 +0000

    Actions: Documentation for cdb54538c05b3f8fcf9c44d5cc13b4740b76188a

--- docs/src-files.js
+++ docs/src-files.js
@@ -1,6 +1,9 @@
-var srcIndex = JSON.parse('{\
-"chuffed_rs":["",[],["lib.rs"]],\
-"conjure_oxide":["",[["rules",[],["mod.rs"]],["solvers",[],["error.rs","kissat.rs","minion.rs","mod.rs"]]],["error.rs","find_conjure.rs","lib.rs","parse.rs"]],\
-"minion_rs":["",[],["ast.rs","error.rs","ffi.rs","lib.rs","run.rs","scoped_ptr.rs"]]\
-}');
+var srcIndex = new Map(JSON.parse('[\
+["chuffed_rs",["",[],["lib.rs"]]],\
+["conjure_core",["",[],["ast.rs","lib.rs","rule.rs","solvers.rs"]]],\
+["conjure_oxide",["",[["rules",[],["base.rs","cnf.rs","constant.rs","minion.rs","mod.rs"]],["solvers",[],["error.rs","kissat.rs","minion.rs","mod.rs"]],["unstable",[["solver_interface",[],["mod.rs"]]],["mod.rs"]],["utils",[],["mod.rs"]]],["error.rs","find_conjure.rs","generate_custom.rs","lib.rs","parse.rs","rewrite.rs"]]],\
+["conjure_rules",["",[],["lib.rs"]]],\
+["minion_rs",["",[],["ast.rs","error.rs","ffi.rs","lib.rs","run.rs","scoped_ptr.rs"]]],\
+["uniplate",["",[],["lib.rs","uniplate.rs"]]]\
+]'));
 createSrcSidebar();
